22cb4091840f2a92946438167b652a3d1b6b009d
hidexir <hidexir@gmail.com>
2021-04-20 11:30:40 +0900

    WIP

--- 3/3.6/main.cpp
+++ 3/3.6/main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//echo "5 10\n 1 2 4 5 11"| ./a.out
 int main() {
   int N,W;
   cin >> N >> W;
@@ -11,8 +12,10 @@ int main() {
 
   bool exist = false;
 
+  //N=5, [11111]
   for (int bit = 0; bit < (1 << N); ++bit) {
     int sum = 0;
+    cout << bitset<5>(bit) << endl;
     for (int i = 0; i < N; ++i) {
       if (bit & (1 << i)) {
         sum += a[i];
