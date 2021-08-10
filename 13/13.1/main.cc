7403333c74690c31f1b23bfd1d26775302576c25
hidexir <hidexir@gmail.com>
2021-08-10 22:37:52 +0900

    WIP

---
+++ 13/13.1/main.cc
@@ -0,0 +1,20 @@
+void serch(const Graph %G, int s) {
+  int N = (int)G.size();
+
+  vector<bool> seen(N, false);
+  queue<int> todo;
+  seen[s] = true;
+  todo.push(s);
+
+  while (!todo.empty()) {
+    int v = todo.front();
+    todo.pop();
+
+    for (int x : G[v]){
+      if(seen[x]) continue;
+      seen[x] = true;
+      todo.push(x);
+    }
+  }
+}
+
